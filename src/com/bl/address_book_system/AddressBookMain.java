3f9103147103c778dbc4e70002a15ccabef1ddbc
mohini patange <patangemohini5@gmail.com>
2022-05-30 21:07:25 +0530

    Welcome message

---
+++ src/com/bl/address_book_system/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bl.address_book_system;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program!!");
+    }
+}
